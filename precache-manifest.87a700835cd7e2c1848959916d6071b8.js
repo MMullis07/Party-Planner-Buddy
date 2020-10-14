35b8d748555d3ee5c0ba6ce3e3f413816ac33d35
mmullis07 <eddiemull311@gmail.com>
2020-10-13 18:24:14 -0700

    Updates

---
+++ precache-manifest.87a700835cd7e2c1848959916d6071b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44197a8cbd2f9b48a9c11598c52dc249",
+    "url": "/Party-Planner-Buddy/index.html"
+  },
+  {
+    "revision": "40fb8f8c94f3ba9dabe0",
+    "url": "/Party-Planner-Buddy/static/css/main.10540457.chunk.css"
+  },
+  {
+    "revision": "ec49247966cceda1be62",
+    "url": "/Party-Planner-Buddy/static/js/2.b2f056da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Party-Planner-Buddy/static/js/2.b2f056da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fb8f8c94f3ba9dabe0",
+    "url": "/Party-Planner-Buddy/static/js/main.9223c2ac.chunk.js"
+  },
+  {
+    "revision": "a22641b4211a79926324",
+    "url": "/Party-Planner-Buddy/static/js/runtime-main.02da7f42.js"
+  }
+]);
